4d242536a86daf458c142ca6258a081e778b3222
gleb50202 <lobko03@list.ru>
2023-05-19 13:43:34 +0300

    Revert "personalized hello"
    This reverts commit ccad57c642d242955038878f96174dad0cb5ec89.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -10,5 +10,6 @@ int main(int argc, char **argv)
 	printf("Now enter your last name: ");
 	gets(last); /* buffer overflow? what's that? */
 	printf("Hello %s %s!\n", first, last);
+	printf("Hello world!\n");
 	return 0;
 }
